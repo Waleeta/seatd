26450ed15e6fd0d3391a657b9883fa9ccf6e2233
Waleeta C <waleetacanon@gmail.com>
2017-01-14 10:53:22 -0600

    Adds devise to Business

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,8 @@
 class Business < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many  :employees
   has_many  :appointments, through: :employees
   has_many  :services
