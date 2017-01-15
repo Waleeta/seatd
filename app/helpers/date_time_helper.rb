07f489ec5ebaaaac91267f67ef2299336d37f97c
Waleeta C <waleetacanon@gmail.com>
2017-01-15 13:11:05 -0600

    Updates available days for appointments on business profile.

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -4,6 +4,30 @@ module DateTimeHelper
     datetime.strftime("%l:%M %P")
   end
 
+  def month
+    month = Date::MONTHNAMES[Date.today.month]
+    # returns current month
+  end
+
+  def today_date
+    today = Date.today
+    today.day.to_s #returns day as number
+  end
+
+  def tomorrow_date
+    tomorrow = today_date.next
+    tomorrow
+  end
+
+  def day_after_tomorrow
+    day_after = tomorrow_date.to_i+1
+    p day_after
+    day_after
+  end
+
+  def full_day
+    month.to_s+" "+today_date.to_s
+  end
 
 end
 
