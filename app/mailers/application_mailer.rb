5e4861993122fee77d809d534803e23ca70000d5
Waleeta C <waleetacanon@gmail.com>
2017-01-18 14:58:39 -0600

    Cfreated mailer files

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "seatd_booked@gmail.com"
+  default from: "from@example.com"
   layout 'mailer'
 end
--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,5 +1,2 @@
 class AppointmentMailer < ApplicationMailer
-  def confirm_email(email, appointment)
-    mail(to: email, subject: 'Someone has booked an appointment!')
-  end
 end
