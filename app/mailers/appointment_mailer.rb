2a3dcbf8782315f92954525c5ab5bb5f58172dc6
Waleeta C <waleetacanon@gmail.com>
2017-01-18 15:27:25 -0600

    Updates mailer, pushing for test

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,2 +1,8 @@
 class AppointmentMailer < ApplicationMailer
+  default from: "seats.booked@gmail.com"
+
+  def confirm_email(email)
+    # @url = ''
+    mail(to: email, subject: 'Someone has been Seatd!!')
+  end
 end
