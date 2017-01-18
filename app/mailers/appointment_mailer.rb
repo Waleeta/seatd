35d34bcbe45870907b2e1108df0965b15a508a61
Samantha Stewart <samantha.stewart726@gmail.com>
2017-01-18 14:47:18 -0600

    Revert "Refactor branch"

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,8 +1,5 @@
 class AppointmentMailer < ApplicationMailer
-  default from: "seatd.booked@gmail.com"
-
-  def confirm_email(email)
-    @url = 'http://localhost:3000/employees/:id/appointments'
+  def confirm_email(email, appointment)
     mail(to: email, subject: 'Someone has booked an appointment!')
   end
 end
